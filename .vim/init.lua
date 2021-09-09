996ca6f3b394deb4ba85c0a405bef68513d92b67
junnplus <junnplus@gmail.com>
2021-09-09 16:59:14 +0800

    update nvim config

--- .vim/init.lua
+++ .vim/init.lua
@@ -58,6 +58,7 @@ set('mouse', 'a')
 set('number', true, window)
 set('cursorline', true, window)
 set('cursorcolumn', true, window)
+set('signcolumn', 'auto:3')
 
 set('textwidth', 120, buffer)
 set('smartindent', true, buffer)
@@ -67,16 +68,18 @@ set('shiftwidth', 4, buffer)
 set('softtabstop', 4, buffer)
 set('tabstop', 4, buffer)
 
-cmd [[ autocmd FileType json,markdown let g:indentLine_conceallevel=0 ]]
-cmd [[ autocmd FileType php setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=120 ]]
-cmd [[ autocmd FileType python setlocal tabstop=4 shiftwidth=4 softtabstop=4 textwidth=120 ]]
-cmd [[ autocmd FileType json setlocal tabstop=4 shiftwidth=4 softtabstop=4 expandtab ]]
-cmd [[ autocmd FileType go setlocal tabstop=8 shiftwidth=8 softtabstop=8 textwidth=120 noexpandtab ]]
-cmd [[ autocmd FileType html,htmldjango,xhtml,haml setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=0 ]]
-cmd [[ autocmd FileType yaml setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=0 expandtab ]]
-cmd [[ autocmd FileType ruby setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=120 ]]
-cmd [[ autocmd FileType less,sass,scss,css setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=120 ]]
-cmd [[ autocmd FileType javascript,javascript.jsx,javascriptreact,typescript,typescriptreact setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab ]]
+-- reopen last position
+cmd([[ autocmd BufReadPost * normal! g`" ]])
+cmd([[ autocmd FileType json,markdown let g:indentLine_conceallevel=0 ]])
+cmd([[ autocmd FileType php setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=120 ]])
+cmd([[ autocmd FileType python setlocal tabstop=4 shiftwidth=4 softtabstop=4 textwidth=120 ]])
+cmd([[ autocmd FileType json setlocal tabstop=4 shiftwidth=4 softtabstop=4 expandtab ]])
+cmd([[ autocmd FileType go setlocal tabstop=8 shiftwidth=8 softtabstop=8 textwidth=120 noexpandtab ]])
+cmd([[ autocmd FileType html,htmldjango,xhtml,haml setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=0 ]])
+cmd([[ autocmd FileType yaml setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=0 expandtab ]])
+cmd([[ autocmd FileType ruby setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=120 ]])
+cmd([[ autocmd FileType less,sass,scss,css setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=120 ]])
+cmd([[ autocmd FileType javascript,javascript.jsx,javascriptreact,typescript,typescriptreact setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab ]])
 
 -- Key bindings
 map('', '<c-a>', 'ggVG$"+y')
@@ -118,6 +121,7 @@ require'nvim-web-devicons'.setup {
 require'bufferline'.setup{
     options = {
         show_buffer_icons = true,
+        diagnostics = 'nvim_lsp',
         indicator_icon = '▎',
         show_close_icon = false,
         modified_icon = '✥',
@@ -156,16 +160,47 @@ map('', '<leader>k', '<Plug>(easymotion-k)', {noremap = false})
 map('', '<leader>h', '<Plug>(easymotion-linebackward)', {noremap = false})
 map('', '<leader>w', '<Plug>(easymotion-w)', {noremap = false})
 map('', '<leader>s', '<Plug>(easymotion-overwin-f)', {noremap = false})
-cmd [[ autocmd User EasyMotionPromptBegin LspStop ]]
-cmd [[ autocmd User EasyMotionPromptEnd LspStart ]]
+cmd([[ autocmd User EasyMotionPromptBegin :lua vim.lsp.diagnostic.disable() ]])
+cmd([[ autocmd User EasyMotionPromptEnd :lua vim.lsp.diagnostic.enable() ]])
 
 -- CHOOSEWIN
 map('n', '-', '<Plug>(choosewin)', {noremap = false})
 
--- LSP
+-- NVIM CMP
+require'cmp'.setup{
+    formatting = {
+        format = function(entry, vim_item)
+            -- set a name for each source
+            vim_item.menu = ({
+                buffer = "[Buf]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[Snip]",
+                nvim_lua = "[Lua]",
+                latex_symbols = "[Latex]",
+            })[entry.source.name]
+            return vim_item
+        end
+    },
+    completion = {
+        completeopt = 'menu,menuone,noinsert',
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'cmp_tabnine' },
+        { name = 'buffer' },
+    }
+}
+local tabnine = require('cmp_tabnine.config')
+tabnine:setup({
+    max_lines = 1000;
+    max_num_results = 20;
+    sort = true;
+})
+
+-- LSPCONFIG
 local nvim_lsp = require'lspconfig'
-cmd [[autocmd ColorScheme * highlight NormalFloat guibg=#1f2335]]
-cmd [[autocmd ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+cmd([[autocmd ColorScheme * highlight NormalFloat guibg=#1f2335]])
+cmd([[autocmd ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]])
 
 local on_attach = function(_, bufnr)
     local function buf_set_keymap(...) api.nvim_buf_set_keymap(bufnr, ...) end
@@ -181,10 +216,10 @@ local on_attach = function(_, bufnr)
     -- buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
     buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
     buf_set_keymap('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
     buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
     buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
     -- buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
     -- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
     -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
@@ -196,16 +231,42 @@ local on_attach = function(_, bufnr)
     -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
     -- buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
-    -- vim.lsp.handlers["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border})
-    -- vim.lsp.handlers["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border})
     -- vim.lsp.handlers["textDocument/definition"] = goto_definition('vsplit')
 end
 
+local signs = { Error = "", Warning = "", Hint = "", Information = "" }
+
+for type, icon in pairs(signs) do
+  local hl = "LspDiagnosticsSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
+local border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+vim.lsp.handlers["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border})
+vim.lsp.handlers["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border})
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  underline = true,
+  update_in_insert = false,
+  virtual_text = { spacing = 4 },
+  severity_sort = true,
+})
+
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pyright', 'tsserver', 'gopls', 'vimls' }
+local servers = { 'clangd', 'pyright', 'tsserver', 'gopls', 'vimls' }
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities.textDocument.completion.completionItem.documentationFormat = { 'markdown', 'plaintext' }
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
+        capabilities = capabilities,
         on_attach = on_attach,
         flags = {
           debounce_text_changes = 150,
@@ -213,6 +274,9 @@ for _, lsp in ipairs(servers) do
     }
 end
 -- vim.cmd [[ autocmd CursorHold,CursorHoldI * lua vim.lsp.diagnostic.show_line_diagnostics({focusable=false}) ]]
+--
+-- LSP COLORS
+require'lsp-colors'.setup{}
 
 -- AIRLINE
 g.airline_left_alt_sep = '❯'
@@ -246,11 +310,10 @@ map('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 g.nvim_tree_side = 'left'
 g.nvim_tree_width = 35
 g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
--- g.nvim_tree_auto_open = 1
 g.nvim_tree_auto_close = 1
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_hijack_cursor = 0
-g.nvim_tree_disable_default_keybindings = 1
+-- g.nvim_tree_disable_default_keybindings = 1
 g.nvim_tree_follow_update_path = 1
 g.nvim_tree_follow = 1
 g.nvim_tree_highlight_opened_files = 1
@@ -262,8 +325,49 @@ require'nvim-tree.events'.on_nvim_tree_ready(function ()
     vim.cmd('wincmd p')
     -- vim.cmd('NvimTreeRefresh')
 end)
--- local tree_cb = require'nvim-tree.config'.nvim_tree_callback
--- g.nvim_tree_bindings = {
---     {key='s', cb=tree_cb('split')},
---     {key='v', cb=tree_cb('vsplit')},
--- }
+g.nvim_tree_icons = {
+    symlink = '',
+    lsp = {
+        hint = '',
+        info = '',
+        warning = '',
+        error = '',
+    }
+}
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+g.nvim_tree_bindings = {
+    s = tree_cb('split'),
+    v = tree_cb('vsplit'),
+}
+--
+
+-- NVIM AUTOPAIRS
+require'nvim-autopairs'.setup{}
+
+-- TROUBLE NVIM
+require'trouble'.setup{}
+
+-- GITSIGNS
+require('gitsigns').setup{
+    signs = {
+        add          = {hl = 'GitSignsAdd'   , text = '▍', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+        change       = {hl = 'GitSignsChange', text = '▍', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+        delete       = {hl = 'GitSignsDelete', text = '-', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+        topdelete    = {hl = 'GitSignsDelete', text = '-', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+        changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    },
+    word_diff = true,
+    current_line_blame = true,
+    count_chars = {
+        [1]   = '1', -- '₁',
+        [2]   = '2', -- '₂',
+        [3]   = '3', -- '₃',
+        [4]   = '4', -- '₄',
+        [5]   = '5', -- '₅',
+        [6]   = '6', -- '₆',
+        [7]   = '7', -- '₇',
+        [8]   = '8', -- '₈',
+        [9]   = '9', -- '₉',
+        ['+'] = '' , -- '₊',
+    }
+}
