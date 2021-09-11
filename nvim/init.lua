3b72819901d98ed1d7a0838a8c1c70dd206e8231
junnplus <junnplus@gmail.com>
2021-09-11 23:45:52 +0800

    use packer

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,41 +1,20 @@
 local g = vim.g
 local cmd = vim.cmd
-local ncmd = vim.api.nvim_command
 --
 -- Leader key
 g.mapleader = [[ ]]
---
+
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
+
 -- reopen last position
 cmd([[ autocmd BufReadPost * normal! g`" ]])
 
-ncmd('filetype plugin indent on')
-cmd([[ autocmd FileType php setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=120 ]])
-cmd([[ autocmd FileType python setlocal tabstop=4 shiftwidth=4 softtabstop=4 textwidth=120 ]])
-cmd([[ autocmd FileType json setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab ]])
-cmd([[ autocmd FileType go setlocal tabstop=8 shiftwidth=8 softtabstop=8 textwidth=120 noexpandtab ]])
-cmd([[ autocmd FileType html,htmldjango,xhtml,haml setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=0 ]])
-cmd([[ autocmd FileType yaml setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=0 expandtab ]])
-cmd([[ autocmd FileType ruby setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=120 ]])
-cmd([[ autocmd FileType less,sass,scss,css setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=120 ]])
-cmd([[ autocmd FileType javascript,javascript.jsx,javascriptreact,typescript,typescriptreact setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab ]])
-
+require('plugins')
 require('core')
-require('plugins.lsp')
-require('plugins.nerdcommenter')
-require('plugins.floaterm')
-require('plugins.tagbar')
-require('plugins.indentline')
-require('plugins.devicons')
-require('plugins.choosewin')
-require('plugins.easymotion')
-require('plugins.cmp')
-require('plugins.treesitter')
-require('plugins.bufferline')
-require('plugins.airline')
-require('plugins.autopairs')
-require('plugins.tree')
-require('plugins.trouble')
-require('plugins.gitsigns')
-require('plugins.todo')
-require('plugins.telescope')
 require('mapping')
+require('filetype')
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,194 @@
+return require('packer').startup(function (use)
+    use { 'folke/tokyonight.nvim' }
+
+    use { 'sainnhe/edge' }
+
+    use { 'dracula/vim' }
+
+    use {
+        'vim-airline/vim-airline',
+        config = function ()
+            require('plugins.airline')
+        end
+    }
+
+    use {
+        'tpope/vim-surround'
+    }
+
+    use {
+        'easymotion/vim-easymotion',
+        config = function ()
+            require('plugins.easymotion')
+        end
+    }
+
+    use {
+        'voldikss/vim-floaterm',
+        config = function ()
+            require('plugins.floaterm')
+        end
+    }
+
+    use {
+        'majutsushi/tagbar',
+        config = function ()
+            require('plugins.tagbar')
+        end
+    }
+
+    use {
+        'terryma/vim-multiple-cursors'
+    }
+
+    use {
+        'tpope/vim-fugitive',
+    }
+
+    use {
+        'scrooloose/nerdcommenter',
+        config = function ()
+            require('plugins.nerdcommenter')
+        end
+    }
+
+    use {
+        'wakatime/vim-wakatime',
+    }
+
+    use {
+        'ternjs/tern_for_vim',
+    }
+
+    use {
+        'Vimjas/vim-python-pep8-indent'
+    }
+
+    use {
+        't9md/vim-choosewin',
+        config = function ()
+            require('plugins.choosewin')
+        end
+    }
+
+    use {
+        'kyazdani42/nvim-web-devicons',
+        config = function ()
+            require('plugins.devicons')
+        end
+    }
+
+    use {
+        'lukas-reineke/indent-blankline.nvim',
+        config = function ()
+            require('plugins.indentline')
+        end
+    }
+
+    use {
+        'folke/todo-comments.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} },
+        config = function ()
+            require('plugins.todo')
+        end
+    }
+
+    use {
+        'windwp/nvim-autopairs',
+        config = function ()
+            require('plugins.autopairs')
+        end
+    }
+
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} },
+        config = function ()
+            require('plugins.telescope')
+        end
+    }
+
+    use {
+        'akinsho/bufferline.nvim',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            require('plugins.bufferline')
+        end
+    }
+
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            require('plugins.tree')
+        end
+    }
+
+    use {
+        'folke/trouble.nvim',
+        config = function ()
+            require('plugins.trouble')
+        end
+    }
+
+    use {
+        'lewis6991/gitsigns.nvim',
+        requires = { 'nvim-lua/plenary.nvim' },
+        config = function ()
+            require('plugins.gitsigns')
+        end
+    }
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+        requires = {
+            'nvim-treesitter/playground',
+            'nvim-treesitter/nvim-treesitter-refactor'
+        },
+        config = function ()
+            require('plugins.treesitter')
+        end
+    }
+
+    use {
+        'tzachar/cmp-tabnine',
+        run = './install.sh',
+    }
+
+    use {
+        'hrsh7th/nvim-cmp',
+        requires = {
+            'hrsh7th/vim-vsnip',
+            'hrsh7th/vim-vsnip-integ',
+            'rafamadriz/friendly-snippets',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-nvim-lsp',
+            'tzachar/cmp-tabnine',
+            'onsails/lspkind-nvim',
+        },
+        config = function ()
+            require('plugins.cmp')
+        end
+    }
+
+    use {
+        'neovim/nvim-lspconfig',
+        requires = {
+            'kabouzeid/nvim-lspinstall',
+            'folke/lsp-colors.nvim',
+            'RishabhRD/nvim-lsputils',
+            'folke/lua-dev.nvim'
+        },
+        config = function ()
+            require('plugins.lsp')
+        end
+    }
+
+    use {
+        'folke/twilight.nvim',
+        config = function ()
+            require('plugins.twilight')
+        end
+    }
+end)
