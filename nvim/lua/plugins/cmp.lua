dbfdd8be5754667f2fe80b5f747524f2ed24751b
ye.sijun <junnplus@gmail.com>
2021-11-05 11:43:46 +0800

    update nvim autopairs

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -70,3 +70,6 @@ tabnine:setup({
     max_num_results = 20;
     sort = true;
 })
+
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
