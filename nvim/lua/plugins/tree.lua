41fc47d0b7ca49d2bf7142c614410c6a0d6f0ac4
ye.sijun <junnplus@gmail.com>
2022-02-26 09:30:24 +0800

    update wezterm

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -59,7 +59,13 @@ require('nvim-tree').setup({
     open_on_setup = true,
 })
 
-require('nvim-tree.events').on_nvim_tree_ready(function()
-    vim.cmd('NvimTreeToggle')
-    vim.cmd('wincmd p')
-end)
+-- require('nvim-tree.events').on_nvim_tree_ready(function()
+--     vim.cmd('NvimTreeToggle')
+--     vim.cmd('wincmd p')
+-- end)
+
+vim.cmd([[
+augroup nvimtree
+  autocmd VimEnter * lua require('nvim-tree').toggle(false, true)
+augroup END
+]])
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,9 @@
 local wezterm = require('wezterm')
 
 return {
-    font = wezterm.font('Hack Nerd Font Mono', { weight = 'Regular', stretch = 'Expanded' }),
+    font = wezterm.font('Hack Nerd Font Mono'),
     font_size = 14,
-    freetype_load_target = 'HorizontalLcd',
-    freetype_render_target = 'HorizontalLcd',
-    allow_square_glyphs_to_overflow_width = 'WhenFollowedBySpace',
-    -- color_scheme = "Dracula+",
     colors = {
-        foreground = '#c0caf5',
-        background = '#24283b',
-        cursor_bg = '#c0caf5',
-        cursor_border = '#c0caf5',
-        cursor_fg = '#24283b',
-        selection_bg = '#364A82',
-        selection_fg = '#c0caf5',
-        ansi = { '#1D202F', '#f7768e', '#9ece6a', '#e0af68', '#7aa2f7', '#bb9af7', '#7dcfff', '#a9b1d6' },
-        brights = { '#414868', '#f7768e', '#9ece6a', '#e0af68', '#7aa2f7', '#bb9af7', '#7dcfff', '#c0caf5' },
         tab_bar = {
             active_tab = {
                 bg_color = '#24283b',
@@ -24,12 +11,16 @@ return {
             },
         },
     },
+    color_scheme = 'tokyonight',
     window_frame = {
-        font_size = 13.0,
+        font_size = 14.0,
     },
+    -- window_decorations = 'RESIZE',
+    -- use_fancy_tab_bar = true,
     use_ime = true, -- fixed chinese input
     default_cwd = '/Users/jun/Documents/workspace',
     keys = {
         { key = 'l', mods = 'CMD', action = 'ShowLauncher' },
+        { key = 'w', mods = 'CMD', action = wezterm.action({ CloseCurrentPane = { confirm = false } }) },
     },
 }
