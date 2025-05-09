996eec481d62c19f7c0f381e5fa18f0f68ebca72
Ye Sijun <junnplus@gmail.com>
2025-05-09 23:51:49 +0900

    update wezterm
    Signed-off-by: Ye Sijun <junnplus@gmail.com>

--- wezterm/event.lua
+++ wezterm/event.lua
@@ -3,41 +3,51 @@ local io = require('io')
 local os = require('os')
 
 wezterm.on('window-visible-text', function(window, pane)
-    local viewport_text = pane:get_lines_as_text()
+  local viewport_text = pane:get_lines_as_text()
 
-    -- Create a temporary file to pass to vim
-    local name = os.tmpname()
-    local f = io.open(name, 'w+')
-    if not f then
-        return
-    end
-    f:write(viewport_text)
-    f:flush()
-    f:close()
+  -- Create a temporary file to pass to vim
+  local name = os.tmpname()
+  local f = io.open(name, 'w+')
+  if not f then
+    return
+  end
+  f:write(viewport_text)
+  f:flush()
+  f:close()
 
-    window:perform_action(
-        wezterm.action.SpawnCommandInNewWindow {
-            args = { 'vi', name },
-        },
-        pane
-    )
+  window:perform_action(
+    wezterm.action.SpawnCommandInNewWindow {
+      args = { 'vi', name },
+    },
+    pane
+  )
 
-    wezterm.sleep_ms(1000)
-    os.remove(name)
+  wezterm.sleep_ms(1000)
+  os.remove(name)
 end)
-
 
 wezterm.on('format-tab-title', function(tab)
-    local pane = tab.active_pane
-    local title = '#' .. tab.tab_index + 1 .. ': ' .. pane.title
-    if pane['domain_name'] then
-        title = title .. ' - (' .. pane.domain_name .. ')'
-    end
-    return title
+  local pane = tab.active_pane
+  local title = '⌘' .. tab.tab_index + 1 .. ': ' .. pane.title
+  if pane['domain_name'] then
+    title = title .. ' - (' .. pane.domain_name .. ')'
+  end
+  return title
 end)
 
-wezterm.on('gui-startup', function()
-    local mux = wezterm.mux
-    local _, _, window = mux.spawn_window {}
-    window:gui_window():maximize()
+wezterm.on('augment-command-palette', function(window, pane)
+  return {
+    {
+      brief = 'Maximize window',
+      action = wezterm.action_callback(function()
+        window:maximize()
+      end),
+    },
+    {
+      brief = 'Open finder',
+      action = wezterm.action_callback(function()
+        wezterm.run_child_process { 'open', pane:get_current_working_dir() }
+      end)
+    }
+  }
 end)
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,85 +3,105 @@ require('event')
 local wezterm = require('wezterm')
 
 return {
-    font = wezterm.font('Hack Nerd Font'),
-    font_size = 14,
-    colors = {
-        tab_bar = {
-            active_tab = {
-                bg_color = '#24283b',
-                fg_color = '#c0caf5',
-            },
-        },
+  font = wezterm.font_with_fallback { 'Hack Nerd Font', 'Sarasa Mono SC' },
+  font_size = 14,
+  colors = {
+    tab_bar = {
+      active_tab = {
+        bg_color = '#1a1b26',
+        fg_color = '#c0caf5',
+      },
     },
-    color_scheme = 'tokyonight',
-    window_frame = {
-        font_size = 14.0,
+  },
+  color_scheme = 'tokyonight',
+  window_frame = {
+    font_size = 13.0,
+  },
+  window_decorations = 'INTEGRATED_BUTTONS|RESIZE',
+  window_padding = {
+    left = 5,
+    right = 5,
+    top = -3,
+    bottom = 0,
+  },
+  tab_max_width = 20,
+  foreground_text_hsb = {
+    hue = 1.0,
+    saturation = 1.0,
+    brightness = 1.4,
+  },
+  -- tempfile=$(mktemp) \
+  -- && curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo \
+  -- && tic -x -o ~/.terminfo $tempfile \
+  -- && rm $tempfile
+  term = 'wezterm',
+  default_cursor_style = 'BlinkingBlock',
+  default_cwd = wezterm.home_dir .. '/Documents/workspace',
+  native_macos_fullscreen_mode = true,
+  -- status_update_interval = 5000,
+  clean_exit_codes = { 0, 1, 130 },
+  -- exit_behavior = 'Close',
+  keys = {
+    { key = 'e', mods = 'CMD', action = wezterm.action({ EmitEvent = 'window-visible-text' }) },
+    { key = 'l', mods = 'CMD', action = wezterm.action({ ShowLauncherArgs = { flags = 'DOMAINS|WORKSPACES|FUZZY' } }) },
+    { key = 'w', mods = 'CMD', action = wezterm.action({ CloseCurrentPane = { confirm = false } }) },
+    { key = 'd', mods = 'CMD', action = wezterm.action({ SplitHorizontal = { domain = 'CurrentPaneDomain' } }) },
+    {
+      key = 'd',
+      mods = 'CMD|SHIFT',
+      action = wezterm.action({ SplitVertical = { domain = 'CurrentPaneDomain' } }),
     },
-    window_decorations = 'INTEGRATED_BUTTONS|RESIZE',
-    window_padding = {
-        left = 5,
-        right = 5,
-        top = -3,
-        bottom = 0,
+    {
+      key = '[',
+      mods = 'CMD',
+      action = wezterm.action({ ActivatePaneDirection = 'Next' }),
     },
-    tab_max_width = 20,
-    foreground_text_hsb = {
-        hue = 1.0,
-        saturation = 1.0,
-        brightness = 1.4,
+    {
+      key = ']',
+      mods = 'CMD',
+      action = wezterm.action({ ActivatePaneDirection = 'Prev' }),
     },
-    -- tempfile=$(mktemp) \
-    -- && curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo \
-    -- && tic -x -o ~/.terminfo $tempfile \
-    -- && rm $tempfile
-    term = 'wezterm',
-    default_cursor_style = 'BlinkingBlock',
-    default_cwd = wezterm.home_dir .. '/Documents/workspace',
-    native_macos_fullscreen_mode = true,
-    -- status_update_interval = 5000,
-    clean_exit_codes = { 0, 1, 130 },
-    -- exit_behavior = 'Close',
-    keys = {
-        { key = 'e', mods = 'CMD', action = wezterm.action({ EmitEvent = 'window-visible-text' }) },
-        { key = 'l', mods = 'CMD', action = wezterm.action({ ShowLauncherArgs = { flags = 'DOMAINS' } }) },
-        { key = 'w', mods = 'CMD', action = wezterm.action({ CloseCurrentPane = { confirm = false } }) },
-        { key = 'd', mods = 'CMD', action = wezterm.action({ SplitHorizontal = { domain = 'CurrentPaneDomain' } }) },
-        {
-            key = 'd',
-            mods = 'CMD|SHIFT',
-            action = wezterm.action({ SplitVertical = { domain = 'CurrentPaneDomain' } }),
-        },
-        {
-            key = '[',
-            mods = 'CMD',
-            action = wezterm.action({ ActivatePaneDirection = 'Next' }),
-        },
-        {
-            key = ']',
-            mods = 'CMD',
-            action = wezterm.action({ ActivatePaneDirection = 'Prev' }),
-        },
-        {
-            key = '>',
-            mods = 'CMD|SHIFT',
-            action = wezterm.action.MoveTabRelative(1),
-        },
-        {
-            key = '<',
-            mods = 'CMD|SHIFT',
-            action = wezterm.action.MoveTabRelative(-1),
-        },
-        { key = 'Enter', mods = 'CMD', action = 'ToggleFullScreen' },
+    {
+      key = '>',
+      mods = 'CMD|SHIFT',
+      action = wezterm.action.MoveTabRelative(1),
     },
-    quote_dropped_files = 'Posix',
-    hyperlink_rules = {
-        {
-            regex = [[\b(https|http)://\S*\b]],
-            format = '$0',
-        },
-        {
-            regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-            format = 'https://www.github.com/$1/$3',
-        }
+    {
+      key = '<',
+      mods = 'CMD|SHIFT',
+      action = wezterm.action.MoveTabRelative(-1),
+    },
+    { key = 'Enter', mods = 'CMD', action = 'ToggleFullScreen' },
+    {
+      key = 'k',
+      mods = 'CMD',
+      action = wezterm.action.ActivateCommandPalette,
+    },
+    {
+      key = 'f',
+      mods = 'SHIFT|CTRL',
+      action = wezterm.action_callback(function(window, pane)
+        window:perform_action(wezterm.action.Search 'CurrentSelectionOrEmptyString', pane)
+        window:perform_action(
+          wezterm.action.Multiple {
+            wezterm.action.CopyMode 'ClearPattern',
+            wezterm.action.CopyMode 'ClearSelectionMode',
+            wezterm.action.CopyMode 'MoveToScrollbackBottom'
+          },
+          pane)
+      end),
+    },
+  },
+  quote_dropped_files = 'Posix',
+  hyperlink_rules = {
+    {
+      regex = [[\b(https|http)://\S*\b]],
+      format = '$0',
+    },
+    {
+      regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+      format = 'https://www.github.com/$1/$3',
     }
+  },
+  ssh_domains = wezterm.default_ssh_domains(),
 }
